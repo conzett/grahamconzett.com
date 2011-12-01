a72253c1244d471dd57319444723882fb6b65140
Graham Conzett <conzett@gmail.com>
2011-12-01 01:31:49 -0500

    Change size of level, add directions and change style of links and background

--- html/run.js
+++ html/run.js
@@ -1,107 +1,107 @@
-var x = new level(38, 28);
+var x = new level(26, 28);
 var color = "#ee3c96";
 
 /* c */
-x.structure[19][7] = color;
-x.structure[19][8] = color;
-x.structure[20][6] = color;
-x.structure[21][6] = color;
-x.structure[22][7] = color;
-x.structure[22][8] = color;
+x.structure[19][1] = color;
+x.structure[19][2] = color;
+x.structure[20][0] = color;
+x.structure[21][0] = color;
+x.structure[22][1] = color;
+x.structure[22][2] = color;
 
 /* o */
-x.structure[19][11] = color;
-x.structure[19][12] = color;
-x.structure[20][10] = color;
-x.structure[20][13] = color;
-x.structure[21][10] = color;
-x.structure[21][13] = color;
-x.structure[22][11] = color;
-x.structure[22][12] = color;
+x.structure[19][5] = color;
+x.structure[19][6] = color;
+x.structure[20][4] = color;
+x.structure[20][7] = color;
+x.structure[21][4] = color;
+x.structure[21][7] = color;
+x.structure[22][5] = color;
+x.structure[22][6] = color;
 
 /* m */
-x.structure[19][15] = color;
-x.structure[19][16] = color;
-x.structure[19][17] = color;
+x.structure[19][9] = color;
+x.structure[19][10] = color;
+x.structure[19][11] = color;
+x.structure[19][13] = color;
+x.structure[20][9] = color;
+x.structure[20][12] = color;
+x.structure[20][14] = color;
+x.structure[21][9] = color;
+x.structure[21][12] = color;
+x.structure[21][14] = color;
+x.structure[22][9] = color;
+x.structure[22][12] = color;
+x.structure[22][14] = color;
+
+/* i */
+x.structure[18][16] = color;
+x.structure[20][16] = color;
+x.structure[21][16] = color;
+x.structure[22][16] = color;
+
+/* n */
+x.structure[19][18] = color;
 x.structure[19][19] = color;
-x.structure[20][15] = color;
 x.structure[20][18] = color;
 x.structure[20][20] = color;
-x.structure[21][15] = color;
 x.structure[21][18] = color;
 x.structure[21][20] = color;
-x.structure[22][15] = color;
 x.structure[22][18] = color;
 x.structure[22][20] = color;
 
-/* i */
-x.structure[18][22] = color;
-x.structure[20][22] = color;
-x.structure[21][22] = color;
-x.structure[22][22] = color;
-
-/* n */
+/* g */
+x.structure[19][23] = color;
 x.structure[19][24] = color;
 x.structure[19][25] = color;
-x.structure[20][24] = color;
-x.structure[20][26] = color;
-x.structure[21][24] = color;
-x.structure[21][26] = color;
+x.structure[20][22] = color;
+x.structure[20][25] = color;
+x.structure[21][22] = color;
+x.structure[21][25] = color;
+x.structure[22][23] = color;
 x.structure[22][24] = color;
-x.structure[22][26] = color;
-
-/* g */
-x.structure[19][29] = color;
-x.structure[19][30] = color;
-x.structure[19][31] = color;
-x.structure[20][28] = color;
-x.structure[20][31] = color;
-x.structure[21][28] = color;
-x.structure[21][31] = color;
-x.structure[22][29] = color;
-x.structure[22][30] = color;
-x.structure[22][31] = color;
-x.structure[23][31] = color;
-x.structure[24][29] = color;
-x.structure[24][30] = color;
+x.structure[22][25] = color;
+x.structure[23][25] = color;
+x.structure[24][23] = color;
+x.structure[24][24] = color;
 
 /* s */
-x.structure[24][11] = color;
-x.structure[24][12] = color;
-x.structure[25][10] = color;
-x.structure[26][12] = color;
+x.structure[24][5] = color;
+x.structure[24][6] = color;
+x.structure[25][4] = color;
+x.structure[26][6] = color;
+x.structure[27][4] = color;
+x.structure[27][5] = color;
+
+/* o */
+x.structure[24][9] = color;
+x.structure[24][10] = color;
+x.structure[25][8] = color;
+x.structure[25][11] = color;
+x.structure[26][8] = color;
+x.structure[26][11] = color;
+x.structure[27][9] = color;
 x.structure[27][10] = color;
-x.structure[27][11] = color;
 
 /* o */
+x.structure[24][14] = color;
 x.structure[24][15] = color;
-x.structure[24][16] = color;
-x.structure[25][14] = color;
-x.structure[25][17] = color;
-x.structure[26][14] = color;
-x.structure[26][17] = color;
+x.structure[25][13] = color;
+x.structure[25][16] = color;
+x.structure[26][13] = color;
+x.structure[26][16] = color;
+x.structure[27][14] = color;
 x.structure[27][15] = color;
-x.structure[27][16] = color;
-
-/* o */
-x.structure[24][20] = color;
-x.structure[24][21] = color;
-x.structure[25][19] = color;
-x.structure[25][22] = color;
-x.structure[26][19] = color;
-x.structure[26][22] = color;
-x.structure[27][20] = color;
-x.structure[27][21] = color;
 
 /* n */
-x.structure[24][24] = color;
-x.structure[24][25] = color;
-x.structure[25][24] = color;
-x.structure[25][26] = color;
-x.structure[26][24] = color;
-x.structure[26][26] = color;
-x.structure[27][24] = color;
-x.structure[27][26] = color;
+x.structure[24][18] = color;
+x.structure[24][19] = color;
+x.structure[25][18] = color;
+x.structure[25][20] = color;
+x.structure[26][18] = color;
+x.structure[26][20] = color;
+x.structure[27][18] = color;
+x.structure[27][20] = color;
 
 
 var game = new game(null, x);
