f7bd33a653727f250e662adb1bc8b1b4744cc4b6
Graham Conzett <conzett@gmail.com>
2012-01-18 02:12:04 -0500

    Update refactored tetris source files

--- html/run.js
+++ html/run.js
@@ -1,107 +1,39 @@
-var x = new level(26, 28);
-var color = "#ee3c96";
+var structure = new Array();
 
-/* c */
-x.structure[19][1] = color;
-x.structure[19][2] = color;
-x.structure[20][0] = color;
-x.structure[21][0] = color;
-x.structure[22][1] = color;
-x.structure[22][2] = color;
+var i = "#ee3c96";
+var o = undefined;
 
-/* o */
-x.structure[19][5] = color;
-x.structure[19][6] = color;
-x.structure[20][4] = color;
-x.structure[20][7] = color;
-x.structure[21][4] = color;
-x.structure[21][7] = color;
-x.structure[22][5] = color;
-x.structure[22][6] = color;
+structure = [
+	[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+	[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+	[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+	[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+	[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+	[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+	[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+	[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+	[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+	[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+	[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+	[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+	[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+	[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+	[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+	[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+	[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+	[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,i,o,o,o,o,o,o,o,o,o],
+	[o,i,i,o,o,i,i,o,o,i,i,i,o,i,o,o,o,o,i,i,o,o,o,i,i,i],
+	[i,o,o,o,i,o,o,i,o,i,o,o,i,o,i,o,i,o,i,o,i,o,i,o,o,i],
+	[i,o,o,o,i,o,o,i,o,i,o,o,i,o,i,o,i,o,i,o,i,o,i,o,o,i],
+	[o,i,i,o,o,i,i,o,o,i,o,o,i,o,i,o,i,o,i,o,i,o,o,i,i,i],
+	[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,i],
+	[o,o,o,o,o,i,i,o,o,i,i,o,o,o,i,i,o,o,i,i,o,o,o,i,i,o],
+	[o,o,o,o,i,o,o,o,i,o,o,i,o,i,o,o,i,o,i,o,i,o,o,o,o,o],
+	[o,o,o,o,o,o,i,o,i,o,o,i,o,i,o,o,i,o,i,o,i,o,o,o,o,o],
+	[o,o,o,o,i,i,o,o,o,i,i,o,o,o,i,i,o,o,i,o,i,o,o,o,o,o]
+]
 
-/* m */
-x.structure[19][9] = color;
-x.structure[19][10] = color;
-x.structure[19][11] = color;
-x.structure[19][13] = color;
-x.structure[20][9] = color;
-x.structure[20][12] = color;
-x.structure[20][14] = color;
-x.structure[21][9] = color;
-x.structure[21][12] = color;
-x.structure[21][14] = color;
-x.structure[22][9] = color;
-x.structure[22][12] = color;
-x.structure[22][14] = color;
-
-/* i */
-x.structure[18][16] = color;
-x.structure[20][16] = color;
-x.structure[21][16] = color;
-x.structure[22][16] = color;
-
-/* n */
-x.structure[19][18] = color;
-x.structure[19][19] = color;
-x.structure[20][18] = color;
-x.structure[20][20] = color;
-x.structure[21][18] = color;
-x.structure[21][20] = color;
-x.structure[22][18] = color;
-x.structure[22][20] = color;
-
-/* g */
-x.structure[19][23] = color;
-x.structure[19][24] = color;
-x.structure[19][25] = color;
-x.structure[20][22] = color;
-x.structure[20][25] = color;
-x.structure[21][22] = color;
-x.structure[21][25] = color;
-x.structure[22][23] = color;
-x.structure[22][24] = color;
-x.structure[22][25] = color;
-x.structure[23][25] = color;
-x.structure[24][23] = color;
-x.structure[24][24] = color;
-
-/* s */
-x.structure[24][5] = color;
-x.structure[24][6] = color;
-x.structure[25][4] = color;
-x.structure[26][6] = color;
-x.structure[27][4] = color;
-x.structure[27][5] = color;
-
-/* o */
-x.structure[24][9] = color;
-x.structure[24][10] = color;
-x.structure[25][8] = color;
-x.structure[25][11] = color;
-x.structure[26][8] = color;
-x.structure[26][11] = color;
-x.structure[27][9] = color;
-x.structure[27][10] = color;
-
-/* o */
-x.structure[24][14] = color;
-x.structure[24][15] = color;
-x.structure[25][13] = color;
-x.structure[25][16] = color;
-x.structure[26][13] = color;
-x.structure[26][16] = color;
-x.structure[27][14] = color;
-x.structure[27][15] = color;
-
-/* n */
-x.structure[24][18] = color;
-x.structure[24][19] = color;
-x.structure[25][18] = color;
-x.structure[25][20] = color;
-x.structure[26][18] = color;
-x.structure[26][20] = color;
-x.structure[27][18] = color;
-x.structure[27][20] = color;
+var x = new level(structure);
 
 
 var game = new game(null, x);
--- html/tests/tests.js
+++ html/tests/tests.js
@@ -7,8 +7,8 @@ $(document).ready(function(){
 	});	
 
 	test("Test level array creation", function() {
-		var testLevel = new level(6,6);	
-		equals(testLevel.structure[5][5], undefined, "We expect the array location to exist, undefined" );		
+		var testLevel = new level(null, 6,6);	
+		equals(testLevel.getStructure()[5][5], undefined, "We expect the array location to exist, undefined" );		
 	});
 	
 	test("Test createPiece() method", function() {
@@ -18,43 +18,8 @@ $(document).ready(function(){
 			return false;
 		}
 		
-		var testPiece = this.level.createPiece();
-		ok(oneOrZero(testPiece.structure[1][1]), "We expect the random piece to be at least this large" );		
-	});
-	
-	test("Test checkInBoundsLeft() method", function() {		
-		
-		var testLevel = new level(6,6, new piece());
-		testLevel.active.left = 0;
-		equals(testLevel.checkInBoundsLeft(), false, "0 units to the left, we expect the value to be false");
-		
-		testLevel.active.left = 1;
-		equals(testLevel.checkInBoundsLeft(), true, "1 units to the left, we expect the value to be true");
-		
-	});
-	
-	test("Test checkInBoundsRight() method", function() {		
-		
-		var testLevel = new level(6,6, new piece());
-		testLevel.active.left = 5;
-		equals(testLevel.checkInBoundsRight(), false, "6 units to the left, we expect the value to be false");
-		
-		testLevel.active.left = 4;
-		equals(testLevel.checkInBoundsRight(), true, "5 units to the left, we expect the value to be true");
-		
-	});
-	
-	test("Test checkInBoundsBottom() method", function() {		
-		
-		var testLevel = new level(6,6, new piece());
-		
-		testLevel.active.left = 0;
-		testLevel.active.top = 5;
-		equals(testLevel.checkInBoundsBottom(), false, "6 units from the top, expect the value to be false");
-		
-		testLevel.active.top = 4;
-		equals(testLevel.checkInBoundsBottom(), true, "5 units from the top, expect the value to be true");
-		
+		var testPiece = this.level.getActive();
+		ok(oneOrZero(testPiece.getStructure()[1][1]), "We expect the random piece to be at least this large" );		
 	});
 
 	test('Test set piece function', function() {
@@ -63,45 +28,55 @@ $(document).ready(function(){
 
 		var testPiece = new piece(
 		    [[1,1,1],
-			[0,1,0]], color
+			[0,1,0]], color, 2, 2
 		);
 
-		testPiece.top = 2;
-		testPiece.left = 2;
-
-		var testLevel = new level(5,4, testPiece);		
+		var testLevel = new level(null, 5,4, testPiece);		
 
 		testLevel.placeActive();
 
-		equals(testLevel.structure[2][2], color, "We expect this location to contain the color " + color );
-		equals(testLevel.structure[2][3], color, "We expect this location to contain the color " + color );
-		equals(testLevel.structure[2][4], color, "We expect this location to contain the color " + color );
-		equals(testLevel.structure[3][2], undefined, "We expect this location to nothing");
-		equals(testLevel.structure[3][3], color, "We expect this location to contain the color " + color );
-		equals(testLevel.structure[3][4], undefined, "We expect this location to nothing");
+		equals(testLevel.getStructure()[2][2], color, "We expect this location to contain the color " + color );
+		equals(testLevel.getStructure()[2][3], color, "We expect this location to contain the color " + color );
+		equals(testLevel.getStructure()[2][4], color, "We expect this location to contain the color " + color );
+		equals(testLevel.getStructure()[3][2], undefined, "We expect this location to nothing");
+		equals(testLevel.getStructure()[3][3], color, "We expect this location to contain the color " + color );
+		equals(testLevel.getStructure()[3][4], undefined, "We expect this location to nothing");
 		
 	});
 
 	test('Test isObstructedLeft function', function() {
 
-		var testPiece = new piece(
-		    [[1,1,1],
-			[0,1,0]]
+		var testPiece = new piece([	[1,1,1],
+									[0,1,0]],
+									null,
+									0,
+									1
 		);
 
-		testPiece.top = 1;
-		testPiece.left = 1;
+		var x = "#336699";
+		var o = undefined;
 
-		var testLevel = new level(5,4, testPiece);	
+		var levelStructure = new Array();
+		levelStructure = [	[x,o,o,o,o],
+							[o,o,o,o,o],
+							[x,o,o,o,o],
+							[o,o,o,o,o]]
 
-		testLevel.structure[1][0] = "#336699";		
+		var testLevel = new level(levelStructure, null,null, testPiece);
+	
 		ok(testLevel.isObstructedLeft(), "We expect this location to return true");
 
-		testLevel.structure[1][0] = undefined;		
+		testLevel.dropActive();
 		ok(!testLevel.isObstructedLeft(), "We expect this location to return false");
 
-		testLevel.structure[2][1] = "#336699";		
+		testLevel.dropActive();	
 		ok(testLevel.isObstructedLeft(), "We expect this location to return true");
+
+		testLevel.moveActiveRight();
+		equals(testLevel.isObstructedLeft(), false, "0 units to the left, we expect the value to be false");
+		
+		testLevel.moveActiveLeft();
+		equals(testLevel.isObstructedLeft(), true, "1 units to the left, we expect the value to be true");
 			
 	});
 
@@ -109,22 +84,31 @@ $(document).ready(function(){
 
 		var testPiece = new piece(
 		    [[1,1,1],
-			[0,1,0]]
+			[0,1,0]],
+			null, 0, 1
 		);
 
-		testPiece.top = 1;
-		testPiece.left = 1;
+		var x = "#336699";
+		var o = undefined;
 
-		var testLevel = new level(5,4, testPiece);		
+		var levelStructure = new Array();
+		levelStructure = [	[o,o,o,o,x],
+							[o,o,o,x,o],
+							[o,o,o,o,o],
+							[o,o,o,o,o]]
 
-		testLevel.structure[1][4] = "#336699";		
+		var testLevel = new level(levelStructure, null,null, testPiece);		
+	
 		ok(testLevel.isObstructedRight(), "We expect this location to return true");
-		
-		testLevel.structure[1][4] = undefined;		
+
+		testLevel.moveActiveLeft();
+			
 		ok(!testLevel.isObstructedRight(), "We expect this location to return false");
 
-		testLevel.structure[2][3] = "#336699";		
+		testLevel.dropActive();
+		
 		ok(testLevel.isObstructedRight(), "We expect this location to return true");
+
 			
 	});
 
@@ -132,64 +116,71 @@ $(document).ready(function(){
 
 		var testPiece = new piece(
 		    [[1,1,1],
-			[0,1,0]]
+			[0,1,0]],
+			null, 0, 1
 		);
 
-		testPiece.top = 1;
-		testPiece.left = 1;
+		var x = "#336699";
+		var o = undefined;
 
-		var testLevel = new level(5,4, testPiece);		
+		var levelStructure = new Array();
+		levelStructure = [	[o,o,o,o,o],
+							[o,o,o,o,o],
+							[o,o,o,o,o],
+							[o,o,x,o,o]]
 
-		testLevel.structure[3][2] = "#336699";		
-		ok(testLevel.isObstructedBottom(), "We expect this location to return true");
-		
-		testLevel.structure[3][2] = undefined;		
+		var testLevel = new level(levelStructure, null,null, testPiece);		
+	
 		ok(!testLevel.isObstructedBottom(), "We expect this location to return false");
 
-		testLevel.structure[2][1] = "#336699";		
+		testLevel.dropActive();
+			
 		ok(testLevel.isObstructedBottom(), "We expect this location to return true");
+
+		testLevel.moveActiveLeft();
+			
+		ok(!testLevel.isObstructedBottom(), "We expect this location to return false");
 			
 	});
 
 	test('Test getFullRows function', function() {
 
-		var testLevel = new level(5,5);
+		var x = "#336699";
+		var o = undefined;
+
+		var levelStructure = new Array();
+		levelStructure = [	[o,o,o,o,o],
+							[x,x,x,x,x],
+							[o,o,o,o,o],
+							[x,x,x,x,x]]
+
+		var testLevel = new level(levelStructure, 5,5);
 		var i;
-
-		testLevel.structure[0][2] = "#336699";
-		
-		for(i = 0; i < testLevel.structure[0].length; i++){
-			testLevel.structure[2][i] = "#336699";	
-		}
-
-		for(i = 0; i < testLevel.structure[0].length; i++){
-			testLevel.structure[4][i] = "#336699";	
-		}
 
 		result = testLevel.getFullRows();
 				
-		equals(result[0], 2, "We expect this to equl 2 the index of the first full row");
-		equals(result[1], 4, "We expect this to equl 4 the index of the next full row");
+		equals(result[0], 1, "We expect this to equal 1 the index of the first full row");
+		equals(result[1], 3, "We expect this to equal 3 the index of the next full row");
 			
 	});
 
 	test('Test clearRows function', function() {
 
-		var testLevel = new level(5,5);
+		var testLevel = new level(null, 5,5);
 		var i;
 		
-		for(i = 0; i < testLevel.structure[0].length; i++){
-			testLevel.structure[2][i] = "#336699";	
+		for(i = 0; i < testLevel.getStructure()[0].length; i++){
+			testLevel.getStructure()[2][i] = "#336699";	
 		}
 
 		testLevel.clearRows([2]);
 
-		for(i = 0; i < testLevel.structure[0].length; i++){
-			equals(testLevel.structure[2][i], undefined, "We expect the space at index " + i + " to be undefined");
+		for(i = 0; i < testLevel.getStructure()[0].length; i++){
+			equals(testLevel.getStructure()[2][i], undefined, "We expect the space at index " + i + " to be undefined");
 		}
 				
 		
-		equals(testLevel.structure.length, 5, "We expect the length to still be 5");
+		equals(testLevel.getStructure().length, 5, "We expect the length to still be 5");
 			
 	});
 	
@@ -199,16 +190,16 @@ $(document).ready(function(){
 
 	test("Test piece array creation", function() {
 		var testPiece = new piece([[true, true], [true, true]]);	
-		equals(testPiece.structure[1][1], true, "We expect the array location to be true" );		
+		equals(testPiece.getStructure()[1][1], true, "We expect the array location to be true" );		
 	});
 	
 	test('Test piece rotate counter-clock-wise', function() {
 		var testPiece = new piece([[true, false], [false, true]]);
 		testPiece.rotate('ccw');
-		equals(testPiece.structure[0][0], false, "We expect this location to be false" );
-		equals(testPiece.structure[0][1], true, "We expect this location to be true" );
-		equals(testPiece.structure[1][0], true, "We expect this location to be true" );
-		equals(testPiece.structure[1][1], false, "We expect this location to be false" );		
+		equals(testPiece.getStructure()[0][0], false, "We expect this location to be false" );
+		equals(testPiece.getStructure()[0][1], true, "We expect this location to be true" );
+		equals(testPiece.getStructure()[1][0], true, "We expect this location to be true" );
+		equals(testPiece.getStructure()[1][1], false, "We expect this location to be false" );		
 	});
 	
 	test('Test "T-piece" rotate clock-wise', function() {
@@ -225,12 +216,12 @@ $(document).ready(function(){
 		
 		testPiece.rotate('cw');
 		
-		equals(testPiece.structure[0][0], 0, "We expect this location to be 0" );
-		equals(testPiece.structure[0][1], 1, "We expect this location to be 0" );
-		equals(testPiece.structure[1][0], 1, "We expect this location to be 0" );
-		equals(testPiece.structure[1][1], 1, "We expect this location to be 1" );
-		equals(testPiece.structure[2][0], 0, "We expect this location to be 0" );
-		equals(testPiece.structure[2][1], 1, "We expect this location to be 0" );
+		equals(testPiece.getStructure()[0][0], 0, "We expect this location to be 0" );
+		equals(testPiece.getStructure()[0][1], 1, "We expect this location to be 0" );
+		equals(testPiece.getStructure()[1][0], 1, "We expect this location to be 0" );
+		equals(testPiece.getStructure()[1][1], 1, "We expect this location to be 1" );
+		equals(testPiece.getStructure()[2][0], 0, "We expect this location to be 0" );
+		equals(testPiece.getStructure()[2][1], 1, "We expect this location to be 0" );
 	});
 	
 	
