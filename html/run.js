81f066f17726d3edf6d7be7893e2bcab2e87bf62
Graham Conzett <conzett@gmail.com>
2012-01-20 01:14:02 -0500

    Remove local tests, update test url, updates tetris.min.js

--- html/run.js
+++ html/run.js
@@ -1,39 +1,39 @@
-var structure = new Array();
+	var structure;
 
-var i = "#ee3c96";
-var o = undefined;
+	var x = "#ee3c96";
+	var o = undefined;
 
-structure = [
-	[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
-	[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
-	[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
-	[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
-	[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
-	[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
-	[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
-	[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
-	[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
-	[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
-	[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
-	[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
-	[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
-	[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
-	[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
-	[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
-	[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
-	[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,i,o,o,o,o,o,o,o,o,o],
-	[o,i,i,o,o,i,i,o,o,i,i,i,o,i,o,o,o,o,i,i,o,o,o,i,i,i],
-	[i,o,o,o,i,o,o,i,o,i,o,o,i,o,i,o,i,o,i,o,i,o,i,o,o,i],
-	[i,o,o,o,i,o,o,i,o,i,o,o,i,o,i,o,i,o,i,o,i,o,i,o,o,i],
-	[o,i,i,o,o,i,i,o,o,i,o,o,i,o,i,o,i,o,i,o,i,o,o,i,i,i],
-	[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,i],
-	[o,o,o,o,o,i,i,o,o,i,i,o,o,o,i,i,o,o,i,i,o,o,o,i,i,o],
-	[o,o,o,o,i,o,o,o,i,o,o,i,o,i,o,o,i,o,i,o,i,o,o,o,o,o],
-	[o,o,o,o,o,o,i,o,i,o,o,i,o,i,o,o,i,o,i,o,i,o,o,o,o,o],
-	[o,o,o,o,i,i,o,o,o,i,i,o,o,o,i,i,o,o,i,o,i,o,o,o,o,o]
-]
+	structure = [
+		[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+		[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+		[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+		[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+		[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+		[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+		[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+		[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+		[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+		[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+		[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+		[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+		[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+		[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+		[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+		[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+		[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o],
+		[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,x,o,o,o,o,o,o,o,o,o],
+		[o,x,x,o,o,x,x,o,o,x,x,x,o,x,o,o,o,o,x,x,o,o,o,x,x,x],
+		[x,o,o,o,x,o,o,x,o,x,o,o,x,o,x,o,x,o,x,o,x,o,x,o,o,x],
+		[x,o,o,o,x,o,o,x,o,x,o,o,x,o,x,o,x,o,x,o,x,o,x,o,o,x],
+		[o,x,x,o,o,x,x,o,o,x,o,o,x,o,x,o,x,o,x,o,x,o,o,x,x,x],
+		[o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,x],
+		[o,o,o,o,o,x,x,o,o,x,x,o,o,o,x,x,o,o,x,x,o,o,o,x,x,o],
+		[o,o,o,o,x,o,o,o,x,o,o,x,o,x,o,o,x,o,x,o,x,o,o,o,o,o],
+		[o,o,o,o,o,o,x,o,x,o,o,x,o,x,o,o,x,o,x,o,x,o,o,o,o,o],
+		[o,o,o,o,x,x,o,o,o,x,x,o,o,o,x,x,o,o,x,o,x,o,o,o,o,o]
+	]
 
-var x = new level(structure);
+	var l = new Level(structure);
 
 
-var game = new game(null, x);
+	var game = new Game(null, l);
